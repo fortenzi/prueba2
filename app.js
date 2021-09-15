361969d5cdb351b1fc2d3d725e5894aef2970bcb
fortenzi <faortenzi@gmail.com>
2021-09-14 21:33:17 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var nombresMujer = ['andi', 'ana', 'flor', 'susy'];
+
+var nombresHombre = ["fede","carlos","ricardo","john"];
+
+var nombresMascotas = ["pipino", "max", "puppy","makana"];
+
+
+module.exports = {
+    nombresMujer,
+    nombresHombre,
+    nombresMascotas,
+};
